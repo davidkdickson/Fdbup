31a4fe94b2c60770fde50254fe0583d9edffbdcd
David Dickson <davidkdicson@gmail.com>
2011-06-19 22:18:04 +1000

    created solution and refactored into modules.

---
+++ src/Fdbup/Fdbup.fs
@@ -0,0 +1,134 @@
+﻿namespace Fdbup
+
+module Script =
+  open System.Text.RegularExpressions
+  open System.IO
+  open System.Reflection
+  open System.Data.SqlClient
+  
+  type Script = {
+    ScriptName : string;
+    Statements : string seq
+  }
+
+  let splitByGo scriptContents = 
+    Regex.Split(scriptContents, "^\\s*GO\\s*$", RegexOptions.IgnoreCase ||| RegexOptions.Multiline) 
+    |> Array.filter(fun x-> x.Trim().Length > 0)
+
+  let getScriptsFileSystem path = seq { 
+    for file in Directory.GetFiles(path) do 
+      let fileInfo = FileInfo(file)        
+      let fileContents = File.ReadAllText(file)        
+      yield { ScriptName = fileInfo.Name; Statements = splitByGo fileContents } 
+  }
+
+  let resoureAsScript (assembly:Assembly) scriptName =
+    let resourceStream = assembly.GetManifestResourceStream(scriptName)
+    use resourceStreamReader = new StreamReader(resourceStream)
+    let contents = resourceStreamReader.ReadToEnd()
+    {ScriptName = scriptName; Statements = splitByGo contents}
+
+  let getScriptsAssembly (assembly:Assembly) filter = 
+    assembly.GetManifestResourceNames() 
+    |> Seq.filter(filter) 
+    |> Seq.map(resoureAsScript assembly)
+
+  let scriptsToExecute allScripts executed =
+    let scripts = allScripts     
+    let scriptsToExec = Set.ofSeq (scripts|> Seq.map(fun s -> s.ScriptName)) - Set.ofSeq(executed)
+    scripts |> Seq.filter(fun x -> (Seq.exists(fun a -> a = x.ScriptName) scriptsToExec))
+    
+  let executeScript script connString = 
+    use connection = new SqlConnection(connString)
+    connection.Open()
+    for s in script.Statements do
+      use command = new SqlCommand(s, connection)
+      command.ExecuteNonQuery() |> ignore     
+
+module Version = 
+  open System.Data.SqlClient
+  
+  type VersionConfig = {
+    ConnectionString : string;
+    VersionTable : string
+  }
+
+  type IVersion =
+    abstract member Check : unit -> bool
+    abstract member Executed : unit -> string seq
+    abstract member Update : string -> unit
+
+  let doesVersionTableExist config = 
+    use conn = new SqlConnection(config.ConnectionString)
+    do conn.Open()
+    let cmdText = sprintf "select count(*) from sys.objects where type='U' and name='%s'" config.VersionTable
+    use comm = new SqlCommand(cmdText, conn)
+    let result = (comm.ExecuteScalar()) :?> int
+    match result with 
+      | 0 -> false 
+      | _ -> true
+
+  let createVersionTable config = 
+    use conn = new SqlConnection(config.ConnectionString)
+    do conn.Open()
+    let cmdText = sprintf @"create table %s (
+      [Id] int identity(1,1) not null constraint PK_SchemaVersions_Id primary key nonclustered ,
+      [ScriptName] nvarchar(255) not null,
+      [Applied] datetime not null)" config.VersionTable
+    use comm = new SqlCommand(cmdText, conn)
+    comm.ExecuteScalar() 
+    |> ignore
+
+  let checkVersionTable exists create config =
+    match exists() with
+      | false -> create config; false
+      | _ -> true
+
+  let executedScripts config = seq {
+    use conn = new SqlConnection(config.ConnectionString)
+    let cmdText = sprintf "select ScriptName from %s" config.VersionTable
+    use comm = new SqlCommand(cmdText, conn)
+    do conn.Open()
+    use reader = comm.ExecuteReader()
+    while reader.Read() do
+      yield reader.GetString 0
+    }
+
+  let storeScript cfg (script:string) = 
+    use conn = new SqlConnection(cfg.ConnectionString)
+    do conn.Open()
+    let cmdText = sprintf "insert into %s (ScriptName, Applied) values (@scriptName, (getutcdate()))" cfg.VersionTable
+    use comm = new SqlCommand(cmdText, conn)
+    comm.Parameters.AddWithValue("scriptName", script) |> ignore
+    comm.ExecuteNonQuery() |> ignore 
+
+module Upgrader = 
+  open Script
+  open Log
+  open Version
+  open System.Data.SqlClient
+  open Printf
+
+  let performUpgrade getScripts executeScript (version:IVersion) = log {
+    try 
+      do! logMessage "Beginning database upgrade." 
+      do! match version.Check() with 
+        | true -> logMessage "Version table exists." 
+        | false -> logMessage"Version table created"
+      let scriptsToExecute = getScripts()
+      match scriptsToExecute |> Seq.length with           
+        | l when l > 0 ->
+          for script in scriptsToExecute do
+            do! logMessage (sprintf "Executing script: %s" script.ScriptName)
+            executeScript script 
+            version.Update script.ScriptName
+        | _ -> do! logMessage "Database up to date. No script executed."
+      return true
+    with 
+      | :? SqlException as e -> 
+        do! logMessage (sprintf "%s %s %s %s" (e.LineNumber.ToString()) e.Procedure (e.Number.ToString()) e.Message)
+        return false
+      | _ as e ->
+        do! logMessage (sprintf "%s" e.Message)
+        return false
+    }
---
+++ src/Fdbup/Log.fs
@@ -0,0 +1,70 @@
+﻿// Module allows logging to be written as a computation expression (Monad).
+// code taken from blog post by Matthew Podwysocki see 
+// http://codebetter.com/matthewpodwysocki/2010/02/02/a-kick-in-the-monads-writer-edition/
+// Also see https://gist.github.com/292392 for full source code.
+
+namespace Fdbup
+module Log =
+  open System
+  open System.Collections.Generic
+
+  type Log<'W, 'T> = |Log of (unit -> 'T * 'W)
+  let runLog (Log l) = l()
+  
+  type IMonoid<'T> =
+    abstract member mempty  : unit -> 'T
+    abstract member mappend : 'T * 'T -> 'T
+
+  type MonoidAssociations private() = 
+    static let associations = new Dictionary<Type, obj>()
+    static member Add<'T>(monoid : IMonoid<'T>) = associations.Add(typeof<'T>, monoid)
+    static member Get<'T>() = 
+      match associations.TryGetValue(typeof<'T>) with 
+        | true, assoc -> assoc :?> IMonoid<'T>
+        | false, _    -> failwithf "Type %O does not have an implementation of IMonoid" <| typeof<'T>
+
+  let mempty<'T> = MonoidAssociations.Get<'T>().mempty
+  let mappend<'T> a b = MonoidAssociations.Get<'T>().mappend(a, b)
+
+  type ListMonoid<'T>() =
+    interface IMonoid<'T list> with
+      member this.mempty() = []
+      member this.mappend(a, b) = a @ b
+
+  MonoidAssociations.Add(new ListMonoid<string>())
+
+  type LogBuilder() =
+    member this.Return<'W,'T>(a : 'T) : Log<'W,'T> = 
+      Log(fun () -> a, mempty())
+    member this.ReturnFrom<'W,'T>(w : Log<'W,'T>) = w
+    member this.Bind<'W,'T,'U>(m : Log<'W,'T>, k : 'T -> Log<'W,'U>)  : Log<'W,'U> =
+      Log(fun () ->
+        let (a, w)  = runLog m
+        let (b, w') = runLog (k a)
+        in  (b, mappend<'W> w w'))
+    member this.Zero<'W>() : Log<'W,unit> = this.Return ()
+    member this.TryWith<'W,'T>(log : Log<'W,'T>, handler : exn -> Log<'W,'T>) =
+      Log(fun () ->
+        try runLog log
+        with e -> runLog (handler e))
+    member this.TryFinally<'W,'T>(log : Log<'W,'T>, compensation : unit -> unit) =
+      Log(fun () ->
+        try runLog log
+        finally compensation())
+    member this.Using<'D,'W,'T when 'D :> IDisposable and 'D : null>(resource : 'D, body : 'D -> Log<'W,'T>) =
+      this.TryFinally(body resource, (fun () -> match resource with null -> () | disp -> disp.Dispose()))
+    member this.Delay<'W,'T>(f : unit -> Log<'W,'T>) =
+      this.Bind(this.Return (), f)
+    member this.Combine<'W,'T>(comp1 : Log<'W,unit>, comp2 : Log<'W,'T>) =
+      this.Bind(comp1, (fun () -> comp2))
+    member this.Yield(log : Log<'W,'T>) = log
+    member this.While<'W>(pred : unit -> bool, body : Log<'W,unit>) =
+      match pred() with 
+      | true -> this.Bind(body, (fun () -> this.While(pred,body))) 
+      | _ -> this.Return ()
+    member this.For<'W,'T>(items : seq<'T>, body : 'T -> Log<'W,unit>) =
+      this.Using(items.GetEnumerator(), (fun enum -> this.While((fun () -> enum.MoveNext()), this.Delay(fun () -> body enum.Current))))
+
+  let log = new LogBuilder()
+
+  let logMessage (msg : string) = Log(fun () -> (), [msg])
